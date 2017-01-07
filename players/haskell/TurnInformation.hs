7c6338320d48404397d2d7c88a8effed1fab5121
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-01-08 01:00:23 +0900

    add a Initial State
    Turn Number, Samurai State, Battle field State
    の初期状態の変数を書いてみましたが，大変残念な感じなので洗練して下さい．

--- players/haskell/TurnInformation.hs
+++ players/haskell/TurnInformation.hs
@@ -7,14 +7,14 @@ type TurnNumber = Int
 
 ---------- 2. Samurai states
 type CurrentPosition = (Int,Int)
-data OrderStatus = Already | Yet
-data ShowingStatus = Show | Hide
+data OrderStatus = Already | Yet deriving (Show,Eq,Ord)
+data ShowingStatus = Show | Hide deriving (Show,Eq,Ord)
 type TreatmentTurn = Int
 
 data SamuraiState = SS { cp :: CurrentPosition,
                          os :: OrderStatus,
                          ss :: ShowingStatus,
-                         tt :: TreatmentTurn }
+                         tt :: TreatmentTurn } deriving (Show,Eq,Ord)
 
 ---------- 3. Battlefield State
 
@@ -28,6 +28,48 @@ data BattlefieldSection =
   | EnemyBattleaxe   -- 5
   | NotOccupied      -- 8
   | NoInromation     -- 9
-
+  deriving (Show,Eq,Ord)
+           
 -- Battlefield State
 type BattlefieldState = M.Map CurrentPosition BattlefieldSection
+
+
+
+-------- Initial State
+---- Turn Number
+turnNumber :: TurnNumber
+turnNumber = 0
+
+---- Samurai States
+fSpear :: SamuraiState
+fSpear = SS (0,0) Yet Show 0
+
+fSwords :: SamuraiState
+fSwords = SS (0,7) Yet Show 0
+
+fAxe :: SamuraiState
+fAxe = SS (7,0) Yet Show 0
+
+eSpear :: SamuraiState
+eSpear = SS (14,14) Yet Show 0
+
+eSwords :: SamuraiState
+eSwords = SS (14,7) Yet Show 0
+
+eAxe :: SamuraiState
+eAxe = SS (7,14) Yet Show 0
+
+data Samurai = Fspear | Fswords | Faxe | Espear | Eswords | Eaxe
+  deriving (Show,Eq,Ord)
+
+type SamuraiStates = M.Map Samurai SamuraiState
+
+samuraiStates :: SamuraiStates
+samuraiStates = M.fromList [(Fspear,fSpear),(Fswords,fSwords),(Faxe,fAxe),(Espear,eSpear),(Eswords,eSwords),(Eaxe,eAxe)]
+
+-- Battlefield State
+battleFieldState :: BattlefieldState
+battleFieldState = foldl
+  (\list (piece,point) -> (M.update (\_ -> Just piece) point) list)
+  (M.fromList [((x,y), NotOccupied) | x <- [1..15], y <- [1..15]])
+  [(FriendSpear, (0,0)), (FriendSwords, (0,7)), (FriendBattleaxe, (7,0)), (EnemySpear, (14,14)), (EnemySwords, (14,7)), (EnemyBattleaxe,(7,14))]
