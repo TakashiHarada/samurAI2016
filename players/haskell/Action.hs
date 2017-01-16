c90b1213dcdd4169b6cd0c6533de9ee1e3834e12
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-01-16 12:14:27 +0900

    typo and add cost functions

--- players/haskell/Action.hs
+++ players/haskell/Action.hs
@@ -2,5 +2,11 @@ module Action where
 
 import qualified Direction as D
 
-data Action = Occupy D.Direction | Move D.Direction | Show | Hyde
+data Action = Occupy D.Direction | Move D.Direction | Show | Hide
   deriving (Show,Eq,Ord)
+
+actionCost :: Action -> Int
+actionCost (Occupy _) = 4
+actionCost (Move _) = 2
+actionCost Show = 1
+actionCost Hide = 1
--- players/haskell/Ordering.hs
+++ players/haskell/Ordering.hs
@@ -8,6 +8,12 @@ import System.IO
 data Order = Order { weapon :: W.Weapon, actions :: [A.Action] }
 
 -- e.g. : Order W.Spear [A.Occupy D.South]
+--        Order W.Spear [A.Occupy D.South, A.Move D.North, A.Hide]
+
+orderCostLimit = 7
 
 sendOrderString :: [Order] -> IO()
 sendOrderString orders = putStrLn "1 1 6 9 0" >>= \_ -> hFlush stdout --FIXME
+
+orderCost :: Order -> Int
+orderCost order = sum $ map A.actionCost (actions order)
