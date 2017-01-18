e7d34e92db854099e9eee2fead20e96d34522a88
Yuki Ishikawa <7023.yu.ki@gmail.com>
2017-01-18 15:37:41 +0900

    TurnInformationToGameData

--- players/haskell/GameData.hs
+++ players/haskell/GameData.hs
@@ -18,18 +18,27 @@ acknowledgementResponseToTheGameInformation :: IO ()
 acknowledgementResponseToTheGameInformation = putStrLn "0" >>= \_ -> hFlush stdout
 
 divideComponent :: String -> GameData
-divideComponent = undefined
-
---divideComponent = \s -> GameData (getTN s) (M.fromList (getSS s)) (M.fromList [getBS s])
+--divideComponent = undefined
+divideComponent s = GameData (getTN s) (M.fromList (getSS s)) (M.fromList [getBS s])
 
 getTN :: String -> T.TurnNumber
-getTN s = read $ head $ lines s
+getTN s = (read.head.lines) s
 
 getSS :: String -> [((A.Army,W.Weapon),S.SamuraiState)]
-getSS = undefined
---getSS s = zip [ (x,y) | x <- A.Army, y <- W.Weapon]  S.SamuraiState ((,)) () () () 
---      where
---       info = words tail lines s
+--getSS = undefined
+getSS s = zip 
+      [ (x,y) | x <- [A.Friend,A.Enemy], y <- [W.Spear,W.Swords,W.Axe]] 
+      [ S.SamuraiState ((map \[x,y] -> (x,y) . (map read) .  take 2 .  words . lines s !! 0),(map \[x,y] -> (x,y) .map read .  take 2 .  words . lines s !! 1))  ((searchPosition pos) !! 2 ) ((searchPosition pos) !! 3) ((searchPosition pos) !! 4)  | pos <- [0..5] ]
+      where
+      searchPosition n = map (take 2 . words . tail . take 7 . lines) s
+
+-- getSS s = zip 
+--       [ (x,y) | x <- [A.Friend,A.Enemy], y <- [W.Spear,W.Swords,W.Axe]] 
+--       [ z | z <- [ zz | zz <- S.SamuraiState (( (searchPosition pos) !! 0),((searchPosition pos) !! 1) ) ((searchPosition pos) !! 2 ) ((searchPosition pos) !! 3) ((searchPosition pos) !! 4) ,pos <- [0..5] ] ]
+--       where
+--       searchPosition n = map (take 2 . words . tail . take 7 . lines) s
 
 getBS :: String -> B.BattlefieldState
-getBS = undefined
+--getBS = undefined
+getBS s  = map (\[x,y] -> (x,y).(map read) .  take 2 .  words . lines) s
+
