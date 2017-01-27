30079f117f454bde65a71dd7447b6003cb61acf4
Kinoshita Shuji <shuji@progsci.info.kanagawa-u.ac.jp>
2017-01-27 12:03:48 +0900

    bugfix of getAttackableAreaOfX.

--- players/haskell/CurrentPosition.hs
+++ players/haskell/CurrentPosition.hs
@@ -1,3 +1,7 @@
 module CurrentPosition where
 
 type CurrentPosition = (Int,Int)
+
+removeOutOfBoard :: [CurrentPosition] -> [CurrentPosition]
+removeOutOfBoard = Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t)
+
--- players/haskell/Tactic.hs
+++ players/haskell/Tactic.hs
@@ -10,6 +10,8 @@ import qualified GameData as G
 import qualified CurrentPosition as C
 import qualified SamuraiStates as SS
 
+import Test.QuickCheck
+
 -- If we attack the Enemy X then True else False, where X is a Weapon.
 canAttackX :: W.Weapon -> G.GameData -> Bool
 canAttackX w (G.GameData _ ss bs) = undefined
@@ -17,13 +19,16 @@ canAttackX w (G.GameData _ ss bs) = undefined
 canAttackSomebody :: G.GameData -> Bool
 canAttackSomebody (G.GameData _ ss bs) = undefined
 
-getAttackableAreaOfX :: W.Weapon -> C.CurrentPosition -> [C.CurrentPosition]
-getAttackableAreaOfX W.Spear (x,y) = Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t)
-  [(x+i,y+j) | i <- [-1..1], j <- [-4..4]] ++ [(x+i,y+j) | i <- [-4..4], j <- [-1..1]] ++ [(x-5,y),(x+5,y),(x,y-5),(x,y+5)]
-getAttackableAreaOfX W.Swords (x,y) =
-  Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t) [(x+i,y+j) | i <- [-3..3], j <- [-3..3], abs i + abs j <= 3]
-getAttackableAreaOfX W.Axe (x,y) =
-  Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t) [(x+i,y+j) | i <- [-2..2], j <- [-2..2]]
+getAttackableArea :: W.Weapon -> C.CurrentPosition -> [C.CurrentPosition]
+getAttackableArea w c = C.removeOutOfBoard $ getAttackableArea' w c
+
+getAttackableArea' :: W.Weapon -> C.CurrentPosition -> [C.CurrentPosition]
+getAttackableArea' W.Spear  (x,y) = [(x+i,y+j) | i <- [-1..1], j <- [-4..4]] ++
+                                    [(x+i,y+j) | i <- [-4..4], j <- [-1..1]] ++
+                                    [(x-5,y),(x+5,y),(x,y-5),(x,y+5)]
+getAttackableArea' W.Swords (x,y) = [(x+i,y+j) | i <- [-3..3], j <- [-3..3], abs i + abs j <= 3]
+getAttackableArea' W.Axe    (x,y) = [(x+i,y+j) | i <- [-2..2], j <- [-2..2], abs i + abs j <= 3]
+
 
 {-
 initial States
@@ -33,3 +38,5 @@ Spear  (0,0)  (14,14)
 Sword  (0,7)  (14,7)
 Axe    (7,0)  (7,14)
 -}
+
+--prop_getAttackableAreaOfX = 
