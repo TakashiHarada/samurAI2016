5f2a59d274fcfe4997adc14372eac161f057f5dc
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-01-29 19:00:41 +0900

    add a detNextOrder example
    この前の石川くんのプログラムを忘れてしまいましたが，
    GameDataの函数は適当に動いています．
    即ち，僕が追加した detNextOrder の例で三つの侍が動作します．

--- players/haskell/Position.hs
+++ players/haskell/Position.hs
@@ -5,3 +5,10 @@ type Position = (Int,Int)
 removeOutOfBoard :: [Position] -> [Position]
 removeOutOfBoard = Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t)
 
+fSpear = (0,0)
+fSword = (0,7)
+fAxe   = (7,0)
+
+eSpear = (14,14)
+eSword = (14,7)
+eAxe   = (7,14)
--- players/haskell/hogePlayer.hs
+++ players/haskell/hogePlayer.hs
@@ -32,37 +32,58 @@ mainLoop gds = do
 
 -- TODO:: Implement!
 detNextOrder :: [GD.GameData] -> O.Order
-detNextOrder gds = O.Order W.Spear [A.Occupy D.South]
+detNextOrder (gd:gds) = case (GD.getTurnNumber gd) `mod` 3 of
+  0 -> O.Order W.Spear  [A.Move D.East, A.Occupy D.South]
+  1 -> O.Order W.Swords [A.Move D.South, A.Occupy D.South]
+  2 -> O.Order W.Axe [A.Occupy D.South, A.Move D.East]
 --detNextOrder gds = O.Order W.Spear [A.Occupy D.South, A.Move D.North]
 
-{-
+hoge = ["14",
+        "0 6 1 0 0",
+        "1 14 0 0 0",
+        "9 12 0 0 0",
+        "-1 -1 1 0 0",
+        "-1 -1 0 0 0",
+        "-1 -1 1 0 0",
+        "0 9 9 9 9 9 9 2 9 9 9 9 9 9 9",
+        "8 9 9 9 9 9 9 9 9 9 9 9 9 9 9",
+        "8 8 9 9 9 9 9 9 9 9 9 9 9 9 9",
+        "8 8 8 9 9 9 9 9 9 9 9 9 9 9 9",
+        "8 8 8 8 9 9 9 9 9 9 9 9 9 9 9",
+        "8 0 0 0 0 9 9 9 9 9 9 9 9 9 9",
+        "0 8 8 8 8 8 9 9 9 9 9 9 9 9 9",
+        "1 8 8 8 8 9 9 9 9 8 9 9 9 9 4",
+        "0 8 8 8 9 9 9 9 8 8 3 9 9 9 9",
+        "0 8 8 9 9 9 9 8 8 8 8 8 9 9 9",
+        "8 8 8 9 9 9 8 8 8 8 8 8 8 9 9",
+        "8 8 8 8 9 8 8 8 2 8 2 8 8 8 9",
+        "1 1 8 8 8 8 8 8 2 2 2 8 8 8 8",
+        "1 1 1 8 8 8 8 8 2 2 2 8 8 8 9",
+        "8 1 1 1 8 8 8 5 2 2 2 8 8 9 3"]
 
-getTurnInfo :: String -> IO()
-getTurnInfo x = do
-  turnNumber <- getLine
-  friend1 <- getLine
-  friend2 <- getLine
-  friend3 <- getLine
-  return ()
-  enemy1 <- getLine
-  enemy2 <- getLine
-  enemy3 <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  _ <- getLine
-  if (turnNumber == 96) then putStrLn "1 1 6 9 0"
-                        else putStrLn "1 1 6 9 0"
-                             >>= \_ -> hFlush stdout >>= \_ -> getTurnInfo x
--}
+{--
+
+14
+0 6 1 0 0
+1 14 0 0 0
+9 12 0 0 0
+-1 -1 1 0 0
+-1 -1 0 0 0
+-1 -1 1 0 0
+0 9 9 9 9 9 9 2 9 9 9 9 9 9 9
+8 9 9 9 9 9 9 9 9 9 9 9 9 9 9
+8 8 9 9 9 9 9 9 9 9 9 9 9 9 9
+8 8 8 9 9 9 9 9 9 9 9 9 9 9 9
+8 8 8 8 9 9 9 9 9 9 9 9 9 9 9
+8 0 0 0 0 9 9 9 9 9 9 9 9 9 9
+0 8 8 8 8 8 9 9 9 9 9 9 9 9 9
+1 8 8 8 8 9 9 9 9 8 9 9 9 9 4
+0 8 8 8 9 9 9 9 8 8 3 9 9 9 9
+0 8 8 9 9 9 9 8 8 8 8 8 9 9 9
+8 8 8 9 9 9 8 8 8 8 8 8 8 9 9
+8 8 8 8 9 8 8 8 2 8 2 8 8 8 9
+1 1 8 8 8 8 8 8 2 2 2 8 8 8 8
+1 1 1 8 8 8 8 8 2 2 2 8 8 8 9
+8 1 1 1 8 8 8 5 2 2 2 8 8 9 3
+
+--}
