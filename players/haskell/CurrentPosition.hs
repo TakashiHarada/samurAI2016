be91c03e68b64d6e2f0c18559ece5d60999ee518
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-02-02 12:49:00 +0900

    Merge branch 'master' of https://github.com/falke727/samurAI
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- players/haskell/CurrentPosition.hs
+++
@@ -1,7 +0,0 @@
-module CurrentPosition where
-
-type CurrentPosition = (Int,Int)
-
-removeOutOfBoard :: [CurrentPosition] -> [CurrentPosition]
-removeOutOfBoard = Prelude.filter (\(s,t) -> s <= 14 && t <= 14 && 0 <= s && 0 <= t)
-
