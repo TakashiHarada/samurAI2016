6043019571ef4f6418a6bf9b1e338cf3ecd0dd20
Kinoshita Shuji <shuji@progsci.info.kanagawa-u.ac.jp>
2017-02-02 16:04:13 +0900

    change types to use GameInformation within detNextOrder.

--- players/haskell/Direction.hs
+++ players/haskell/Direction.hs
@@ -7,3 +7,9 @@ directionID South = 1
 directionID East  = 2
 directionID North = 3
 directionID West  = 4
+
+reverse :: Direction -> Direction
+reverse South = North
+reverse East  = West
+reverse North = South
+reverse West  = East
--- players/haskell/hogePlayer.hs
+++ players/haskell/hogePlayer.hs
@@ -16,24 +16,24 @@ main :: IO ()
 main = do
   gameInfo <- GI.readGameInformation
   GI.respondToTheGameInformation 
-  mainLoop []
+  mainLoop [] gameInfo
   return ()
 
-mainLoop :: [GD.GameData] -> IO()
-mainLoop gds = do
+mainLoop :: [GD.GameData] -> GI.GameInformation -> IO()
+mainLoop gds gi = do
   gd <- fmap GD.divideComponent $ sequence $ take 22 $ repeat getLine
-  O.sendOrderString $ detNextOrder (gd:gds)
+  O.sendOrderString $ detNextOrder (gd:gds) gi
   if (TN.isFinalTurn $ GD.getTurnNumber gd)
     then do
       hFlush stdout
       return ()
     else do
       hFlush stdout
-      mainLoop (gd:gds)
+      mainLoop (gd:gds) gi
 
 -- TODO:: Implement!
-detNextOrder :: [GD.GameData] -> O.Order
-detNextOrder (gd:gds) = case (GD.getTurnNumber gd) `mod` 3 of
+detNextOrder :: [GD.GameData] -> GI.GameInformation -> O.Order
+detNextOrder (gd:gds) gi = case (GD.getTurnNumber gd) `mod` 3 of
   0 -> O.Order W.Spear  [A.Move D.East, A.Occupy D.South]
   1 -> O.Order W.Swords [A.Move D.South, A.Occupy D.South]
   2 -> O.Order W.Axe [A.Occupy D.South, A.Move D.East]
@@ -44,6 +44,13 @@ hogeTurn 1 = O.Order W.Spear [A.Occupy D.East, A.Move D.East]
 hogeTurn 3 = O.Order W.Axe [A.Move D.South, A.Move D.West, A.Move D.West]
 hogeTurn 5 = O.Order W.Swords [A.Move D.East, A.Move D.East, A.Move D.East]
 hogeTurn _ = undefined
+
+-- FIXME:: may be moved to util?
+{-
+reverseOrder :: GI.GameInformation -> O.Order -> O.Order
+reverseOrder GI.First x = x
+reverseOrder GI.Second x = O.reverse x 
+-}
 
 inputExample =
   ["14",
