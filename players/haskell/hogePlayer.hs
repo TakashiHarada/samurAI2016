9aab2228dbdf16238b0836f1e7f865b270a93e2c
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-02-02 12:45:10 +0900

    add comments

--- players/haskell/hogePlayer.hs
+++ players/haskell/hogePlayer.hs
@@ -10,6 +10,7 @@ import qualified GameData as GD
 import qualified Weapon as W
 import qualified Action as A
 import qualified Direction as D
+import qualified Position as P
        
 main :: IO ()
 main = do
@@ -38,6 +39,12 @@ detNextOrder (gd:gds) = case (GD.getTurnNumber gd) `mod` 3 of
   2 -> O.Order W.Axe [A.Occupy D.South, A.Move D.East]
 --detNextOrder gds = O.Order W.Spear [A.Occupy D.South, A.Move D.North]
 
+hogeTurn :: Int -> O.Order
+hogeTurn 1 = O.Order W.Spear [A.Occupy D.East, A.Move D.East]
+hogeTurn 3 = O.Order W.Axe [A.Move D.South, A.Move D.West, A.Move, D.West]
+hogeTurn 5 = O.Order W.Swords [A.Move D.East, A.Move D.East, A.Move East]
+hogeTurn _ = undefined
+
 hoge = ["14",
         "0 6 1 0 0",
         "1 14 0 0 0",
@@ -60,6 +67,32 @@ hoge = ["14",
         "1 1 8 8 8 8 8 8 2 2 2 8 8 8 8",
         "1 1 1 8 8 8 8 8 2 2 2 8 8 8 9",
         "8 1 1 1 8 8 8 5 2 2 2 8 8 9 3"]
+
+getPosition :: [GD.GameData] -> W.Weapon -> Maybe P.Position
+getPosition = undefined
+
+isDangerousPlace :: P.Position -> GD.GameData -> Bool
+isDangerousPlace = undefined
+
+dangerousPlaces :: [GD.GameData] -> [P.Position]
+dangerousPlaces = undefined
+
+canAttack :: W.Weapon -> G.GameData -> Bool
+canAttack = undefined
+
+canAttackIn2 :: W.Weapon -> G.GameData -> Bool
+canAttackIn2 = undefined
+
+canAttackIn3 :: W.Weapon -> G.GameData -> Bool
+canAttackIn3 = undefined
+
+attackOrder :: W.Weapon -> G.GameData -> O.Order
+attackOrder = undefined
+
+occupyOrder :: W.Weapon -> G.GameData -> O.Order
+occupyOrder = undefined
+
+
 
 {--
 
