59aa865ae58c37cad6fa19da8055bcd759d4b011
falke727 <r201470039hs@kanagawa-u.ac.jp>
2017-02-09 11:36:47 +0900

    implement the easy pattern of guessPosition

--- players/haskell/GuessPosition.hs
+++ players/haskell/GuessPosition.hs
@@ -10,10 +10,17 @@ import qualified Position as P
 import qualified HomePosition as HP
 import qualified Weapon as W
 import qualified AttackableArea as AA
+import qualified ShowingStatus as Sh
 import qualified SamuraiStates as SS
 import qualified CanActionSamurai as CAS
 import qualified GetShowingEnemy as GS
 
-
-guessPostion :: W.Weapon -> [GD.GameData] -> P.Position
-guessPostion w (gd:gds) = undefined
+-- 武器 w の敵の位置を推測する函数
+-- 武器 w の敵が見えている(w == Sh.Show)場合は，見えている位置を返す
+guessPostion :: W.Weapon -> GD.GameData -> P.Position -> P.Position
+guessPostion W.Spear (GD.GameData _ ss _) _ | SS.getSamuraiShowingStatus sp ss == Sh.Show = SS.getSamuraiPosition sp ss where sp = (A.Enemy, W.Spear)
+guessPostion W.Spear (GD.GameData _ ss bs) epos = undefined
+guessPostion W.Swords (GD.GameData _ ss _) _ | SS.getSamuraiShowingStatus sw ss == Sh.Show = SS.getSamuraiPosition sw ss where sw = (A.Enemy, W.Swords)
+guessPostion W.Swords (GD.GameData _ ss bs) epos = undefined
+guessPostion W.Axe (GD.GameData _ ss _) _ | SS.getSamuraiShowingStatus axe ss == Sh.Show = SS.getSamuraiPosition axe ss where axe = (A.Enemy, W.Swords)
+guessPostion W.Axe gd epos = undefined
