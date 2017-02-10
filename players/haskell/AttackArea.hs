8f1cf48b1cea49bcf27391ab47c4506911b05468
Yuki Ishikawa <7023.yu.ki@gmail.com>
2017-02-10 12:58:17 +0900

    push AttackArea.hs

--- players/haskell/AttackArea.hs
+++ players/haskell/AttackArea.hs
@@ -8,18 +8,41 @@ import qualified GameData as G
 import qualified Ordering as O
 import Data.Char
 
-
 attackArea :: P.Position -> O.Order -> [P.Position]
-attackArea (x,y) (Order W.Spear (f:b))
-         | f==A.Occupy D.South = [(x,y+1),(x,y+2),(x,y+3),(x,y+4)] 
-           f==A.Occupy D.East  = [(x+1,y),(x+2,y),(x+3,y),(x+4,y)] 
-           f==A.Occupy D.North = [(x,y-1),(x,y-2),(x,y-3),(x,y-4)] 
-           f==A.Occupy D.West  = [(x-1,y),(x-2,y),(x-3,y),(x-4,y)]
-           f==A.Move dir = moving dir 
-                    
-attackArea W.Swords (x,y) ord = undefined
-attackArea W.Axe (x,y) ord = undefined
+attackArea _ (O.Order _ []) = []
+--attackArea (x,y) _ | not(0 <= x && x <= 14) || not(0 <= y && y <= 14) = []
+attackArea (x,y) (O.Order W.Spear (f:b))
+         | f == A.Occupy D.South = [ (a,b) | (a,b) <- [(x,y+1),(x,y+2),(x,y+3),(x,y+4)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.East  = [ (a,b) | (a,b) <- [(x+1,y),(x+2,y),(x+3,y),(x+4,y)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.North = [ (a,b) | (a,b) <- [(x,y-1),(x,y-2),(x,y-3),(x,y-4)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.West  = [ (a,b) | (a,b) <- [(x-1,y),(x-2,y),(x-3,y),(x-4,y)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | not $ f >= A.Show = attackArea (moving (x,y) f) (O.Order W.Spear b)
+         | otherwise = attackArea (x,y) (O.Order W.Spear b)
+         where    
+            home = [(0,0),(0,7),(7,0),(14,14),(14,7),(7,14)]         
 
+attackArea (x,y) (O.Order W.Swords (f:b))
+         | f == A.Occupy D.South = [ (a,b) | (a,b) <- [(x,y+2),(x,y+1),(x+1,y+1),(x+1,y),(x+2,y)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.East  = [ (a,b) | (a,b) <- [(x+2,y),(x+1,y),(x+1,y-1),(x,y-1),(x,y-2)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.North = [ (a,b) | (a,b) <- [(x,y-2),(x,y-1),(x-1,y-1),(x-1,y),(x-2,y)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.West  = [ (a,b) | (a,b) <- [(x-2,y),(x-1,y),(x-1,y+1),(x,y+1),(x,y+1)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | not $ f >= A.Show = attackArea (moving (x,y) f) (O.Order W.Swords b)
+         | otherwise = attackArea (x,y) (O.Order W.Swords b)
+         where    
+            home = [(0,0),(0,7),(7,0),(14,14),(14,7),(7,14)]         
 
-moving :: Position -> Order -> Position
-moving (x,y) (Order _ (f:b)) = step
+attackArea (x,y) (O.Order W.Axe (f:b))
+         | f == A.Occupy D.South = [ (a,b) | (a,b) <- [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.East  = [ (a,b) | (a,b) <- [(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1),(x,y-1),(x-1,y-1)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.North = [ (a,b) | (a,b) <- [(x+1,y+1),(x+1,y),(x+1,y-1),(x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | f == A.Occupy D.West  = [ (a,b) | (a,b) <- [(x+1,y-1),(x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1)],0 <= a && a <= 14,0 <= b && b <= 14,not $ elem (a,b) home]
+         | not $ f >= A.Show = attackArea (moving (x,y) f) (O.Order W.Axe b)
+         | otherwise = attackArea (x,y) (O.Order W.Axe b)
+         where    
+            home = [(0,0),(0,7),(7,0),(14,14),(14,7),(7,14)]
+
+moving :: P.Position -> A.Action -> P.Position
+moving (x,y) (A.Move D.South) = (x,y+1)
+moving (x,y) (A.Move D.East)  = (x+1,y)
+moving (x,y) (A.Move D.North) = (x,y-1)
+moving (x,y) (A.Move D.West)  = (x-1,y)
