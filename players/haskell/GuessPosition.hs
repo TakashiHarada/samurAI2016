4d2a2375935a9214cf53a25022781bcb322d0d96
Kinoshita Shuji <shuji@progsci.info.kanagawa-u.ac.jp>
2017-02-11 14:26:50 +0900

    avoid warnings

--- players/haskell/GuessPosition.hs
+++ players/haskell/GuessPosition.hs
@@ -1,18 +1,18 @@
 module GuessPosition where
 
-import Data.List
-import Control.Applicative
+--import Data.List
+--import Control.Applicative
 
 import qualified GameData as GD
 import qualified Army as A
 import qualified GameInformation as GI
 import qualified Position as P
-import qualified HomePosition as HP
+--import qualified HomePosition as HP
 import qualified Weapon as W
-import qualified AttackableArea as AA
+--import qualified AttackableArea as AA
 import qualified ShowingStatus as Sh
 import qualified SamuraiStates as SS
-import qualified CanActionSamurai as CAS
+--import qualified CanActionSamurai as CAS
 import qualified GetShowingEnemy as GS
 import qualified BattlefieldState as B
 
@@ -20,11 +20,13 @@ import qualified BattlefieldState as B
 -- 敵が見えている(w == Sh.Show)場合は,見えている位置を返す
 guessEnemyPositions :: GI.GameInformation ->
                        [GD.GameData] -> P.EnemyPosition -> P.EnemyPosition
+guessEnemyPositions _ [] (sp,ax,sw) = (sp,ax,sw)
 guessEnemyPositions gi ((GD.GameData tn ss bs):gds) (sp,ax,sw) = (sp',ax',sw')
   where
     isSpearShow  = SS.getSamuraiShowingStatus (A.Enemy, W.Spear)  ss == Sh.Show
     isAxeShow    = SS.getSamuraiShowingStatus (A.Enemy, W.Axe)    ss == Sh.Show
     isSwordsShow = SS.getSamuraiShowingStatus (A.Enemy, W.Swords) ss == Sh.Show
+    --isSpearStay  = 
     diff = B.diffBattlefieldState (GD.getBattlefieldState (head gds)) bs 
     sp' = if isSpearShow  then SS.getSamuraiPosition (A.Enemy, W.Spear) ss
                           else guessPositionFromLog gi W.Spear diff sp
@@ -36,9 +38,10 @@ guessEnemyPositions gi ((GD.GameData tn ss bs):gds) (sp,ax,sw) = (sp',ax',sw')
 -- if not Sh.Show. guess.
 guessPositionFromLog :: GI.GameInformation -> W.Weapon ->
                         B.BattlefieldState -> P.Position -> P.Position
+-- I'm First = Enemy is Second = Enemy is likely to move North and West.
 guessPositionFromLog GI.First  w diff p = head (getMovablePositions p)
+-- I'm Second = Enemy is First = Enemy is likely to move South and East.
 guessPositionFromLog GI.Second w diff p = head (getMovablePositions p)
--- FIXME:: 'head' should be changed!
 
 -- x+1 = East, x-1 = West, y+1 = South, y-1 = North
 getMovablePositions :: P.Position -> [P.Position]
