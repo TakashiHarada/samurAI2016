7718c6c09128f48c3140014ce51b9fddcab2ec30
Kinoshita Shuji <shuji@progsci.info.kanagawa-u.ac.jp>
2017-02-11 15:50:29 +0900

    implement simple guessEnemyPositions

--- players/haskell/GuessPosition.hs
+++ players/haskell/GuessPosition.hs
@@ -13,36 +13,46 @@ import qualified Weapon as W
 import qualified ShowingStatus as Sh
 import qualified SamuraiStates as SS
 --import qualified CanActionSamurai as CAS
-import qualified GetShowingEnemy as GS
+--import qualified GetShowingEnemy as GS
 import qualified BattlefieldState as B
+import qualified OrderStatus as OS
 
 -- 敵の位置を推測する函数
 -- 敵が見えている(w == Sh.Show)場合は,見えている位置を返す
 guessEnemyPositions :: GI.GameInformation ->
                        [GD.GameData] -> P.EnemyPosition -> P.EnemyPosition
 guessEnemyPositions _ [] (sp,ax,sw) = (sp,ax,sw)
-guessEnemyPositions gi ((GD.GameData tn ss bs):gds) (sp,ax,sw) = (sp',ax',sw')
+guessEnemyPositions gi ((GD.GameData _ ss bs):gds) (sp,ax,sw) = (sp',ax',sw')
   where
-    isSpearShow  = SS.getSamuraiShowingStatus (A.Enemy, W.Spear)  ss == Sh.Show
-    isAxeShow    = SS.getSamuraiShowingStatus (A.Enemy, W.Axe)    ss == Sh.Show
-    isSwordsShow = SS.getSamuraiShowingStatus (A.Enemy, W.Swords) ss == Sh.Show
-    --isSpearStay  = 
+    isShow :: W.Weapon -> Bool
+    isShow w = SS.getSamuraiShowingStatus (A.Enemy, w) ss == Sh.Show
+    isStay :: W.Weapon -> Bool
+    isStay w = SS.getSamuraiOrderStatus (A.Enemy, w) ss == OS.Yet
+
     diff = B.diffBattlefieldState (GD.getBattlefieldState (head gds)) bs 
-    sp' = if isSpearShow  then SS.getSamuraiPosition (A.Enemy, W.Spear) ss
-                          else guessPositionFromLog gi W.Spear diff sp
-    ax' = if isAxeShow    then SS.getSamuraiPosition (A.Enemy, W.Axe) ss
-                          else guessPositionFromLog gi W.Axe diff sp
-    sw' = if isSwordsShow then SS.getSamuraiPosition (A.Enemy, W.Swords) ss
-                          else guessPositionFromLog gi W.Swords diff sp
+    sp' = if isStay W.Spear then sp
+          else if isShow W.Spear
+                 then SS.getSamuraiPosition (A.Enemy, W.Spear) ss
+                 else guessPositionFromLog gi W.Spear diff sp
+    ax' = if isStay W.Axe then ax
+          else if isShow W.Axe
+                 then SS.getSamuraiPosition (A.Enemy, W.Axe) ss
+                 else guessPositionFromLog gi W.Axe diff sp
+    sw' = if isStay W.Swords then sw
+          else if isShow W.Swords
+                 then SS.getSamuraiPosition (A.Enemy, W.Swords) ss
+                 else guessPositionFromLog gi W.Swords diff sp
                               
 -- if not Sh.Show. guess.
 guessPositionFromLog :: GI.GameInformation -> W.Weapon ->
                         B.BattlefieldState -> P.Position -> P.Position
 -- I'm First = Enemy is Second = Enemy is likely to move North and West.
-guessPositionFromLog GI.First  w diff p = head (getMovablePositions p)
+guessPositionFromLog GI.First  w diff (x,y) = (x-1,y-1)
 -- I'm Second = Enemy is First = Enemy is likely to move South and East.
-guessPositionFromLog GI.Second w diff p = head (getMovablePositions p)
+guessPositionFromLog GI.Second w diff (x,y) = (x+1,y+1)
 
--- x+1 = East, x-1 = West, y+1 = South, y-1 = North
+-- x+1 = East,  x-1 = West,
+-- y+1 = South, y-1 = North
 getMovablePositions :: P.Position -> [P.Position]
-getMovablePositions (x,y) = P.removeOutOfBoard [(x,y+1),(x,y-1),(x+1,y),(x-1,y)]
+getMovablePositions (x,y) = P.removeOutOfBoard [(x+1,y),(x,y+1),(x-1,y),(x,y-1)]
+-- E, S, W, N
